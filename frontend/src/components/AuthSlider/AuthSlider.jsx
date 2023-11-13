6787216fbd3b56030d37901d663546a9bb82cc3c
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-11-13 23:51:44 +0530

    Added switching to auth-slider

--- frontend/src/components/AuthSlider/AuthSlider.jsx
+++ frontend/src/components/AuthSlider/AuthSlider.jsx
@@ -1,18 +1,51 @@
 import PropTypes from "prop-types";
 import React from "react";
+import { useReducer } from "react";
 import "./AuthSlider.css";
 
-export const AuthSlider = ({ property1, className, onClick }) => {
+
+export const AuthSlider = ({ property1, className }) => {
+  const [state, dispatch] = useReducer(reducer, {
+    property1: property1 || "default",
+  });
+
   return (
-    <div className={`auth-slider ${className}`} onClick={onClick}>
+    <div
+      className={`auth-slider ${className}`}
+      onClick={() => {
+        dispatch("click");
+      }}
+    >
       <div className="group">
-        <div className={`rectangle property-1-0-${property1}`} />
+        <div className={`rectangle ${state.property1}`} />
       </div>
       <div className="text-wrapper">LOGIN</div>
       <div className="div">SIGNUP</div>
     </div>
   );
 };
+
+function reducer(state, action) {
+  if (state.property1 === "default") {
+    switch (action) {
+      case "click":
+        return {
+          property1: "variant-2",
+        };
+    }
+  }
+
+  if (state.property1 === "variant-2") {
+    switch (action) {
+      case "click":
+        return {
+          property1: "default",
+        };
+    }
+  }
+
+  return state;
+}
 
 AuthSlider.propTypes = {
   property1: PropTypes.oneOf(["variant-2", "default"]),
