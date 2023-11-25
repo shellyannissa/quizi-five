9ff5f5e762ad6fa4735b653d6699c0b7416797b9
Ruben Jacob <rubenjacob365@gmail.com>
2023-11-26 01:08:58 +0530

    ruben

--- frontend/src/screens/UserHome/UserHome.jsx
+++ frontend/src/screens/UserHome/UserHome.jsx
@@ -63,6 +63,7 @@ export const UserHome = ({property}) => {
   
   useEffect(() => {
     getAvailableQuizzes();
+    getRegisteredQuizzes();
   }, []);
 
   // const [quizList, setQuizList] = useState(registered);
